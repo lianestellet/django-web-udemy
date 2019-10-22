316d9e6a4b913ca84565bb5cbbaf1413d713ddc4
pablostellet <pablorstellet@Gmail.com>
2019-10-21 21:39:21 -0300

    #19 showing a list from database into views

--- my_app/todo_list/views.py
+++ my_app/todo_list/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
+from .models import List
 
 def home(request):
-    return render(request, 'home.html', {})
+    all_items = List.objects.all
+    return render(request, 'home.html', { 'all_items': all_items })
 
 def about(request):
-    my_name = "John Snow"
-    return render(request, 'about.html', { 'name': my_name })
+    context = { 'first_name': 'John', 'last_name': 'Snow' }
+    return render(request, 'about.html', context)
