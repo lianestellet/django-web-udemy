7651f726eb4d26dbe6cbe7c9f9fa3e279a45c031
pablostellet <pablorstellet@Gmail.com>
2019-10-22 09:33:00 -0300

    #28 updating items

--- my_app/todo_list/urls.py
+++ my_app/todo_list/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('about/', views.about, name='about'),
     path('delete/<list_id>', views.delete, name='delete'),
     path('change_status/<list_id>', views.change_status, name='change_status'),
+    path('edit/<list_id>', views.edit, name='edit'),
+
 ]
--- my_app/todo_list/views.py
+++ my_app/todo_list/views.py
@@ -34,3 +34,20 @@ def change_status(request, list_id):
     item.completed = not item.completed
     item.save()
     return redirect('home')
+
+def edit(request, list_id):
+    if request.method == 'POST':
+        item = List.objects.get(pk=list_id)
+        form = ListForm(request.POST or None, instance=item)
+
+        if form.is_valid():
+            form.save()
+            all_items = List.objects.all
+            messages.success(request, ('Item has been edited!'))
+            return redirect('home')
+
+    else:
+        item = List.objects.get(pk=list_id)
+        return render(request, 'edit.html', { 'item': item })
+
+    
